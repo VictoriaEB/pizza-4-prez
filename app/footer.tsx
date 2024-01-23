c33c860cc927d511353649cc9cca4d797937b57d
VictoriaEB <esvictoria1995@gmail.com>
2024-01-23 12:46:05 -0700

    Added links to footer icons.

--- app/footer.tsx
+++ app/footer.tsx
@@ -11,11 +11,21 @@ export default function Footer() {
     <footer className="border-t border-white bg-stone-800 text-white grid px-4 py-6 gap-6 justify-items-center text-center padding">
       <div className="text-xl font-extrabold">PIZZA</div>
       <div className="inline-flex gap-6 text-2xl">
-        <FaFacebook />
-        <FaTwitter />
-        <FaInstagram />
-        <FaYoutube />
-        <FaLinkedin />
+        <a href="https://www.facebook.com/">
+          <FaFacebook />
+        </a>
+        <a href="https://twitter.com/?lang=en">
+          <FaTwitter />
+        </a>
+        <a href="https://www.instagram.com/">
+          <FaInstagram />
+        </a>
+        <a href="https://www.youtube.com/">
+          <FaYoutube />
+        </a>
+        <a href="https://www.linkedin.com/">
+          <FaLinkedin />
+        </a>
       </div>
     </footer>
   );
